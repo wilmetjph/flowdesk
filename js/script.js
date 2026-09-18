eb5e6448d64ec6add428d0e1bfbd7b50f6880959
wilmetjph <wilmetjph@gmail.com>
2026-09-18 23:18:17 +0200

    Update features

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+const start = document.querySelectorAll('.start');
+
+start.forEach(start => start.addEventListener("click", () => {
+	if (start.textContent == "Get started") {
+		start.textContent = "Let's go!";
+	} else {
+		start.textContent = "Get started";
+	}
+}));
+
 const discount = document.getElementById("discount");
 const description = document.querySelectorAll(".description");
 
@@ -12,7 +22,30 @@ discount.addEventListener("click", function() {
 
 const nav = document.querySelector(".navigation");
 const burger = document.getElementById("burger");
+const link = document.querySelectorAll("a");
+const questions = document.querySelectorAll(".faq_questions")
 
 burger.addEventListener("click", () => {
 	nav.classList.toggle('open');
 });
+
+link.forEach(link => link.addEventListener("click", () => {
+	if (nav.classList.contains('open')) {
+		nav.classList.toggle('open');
+	}
+}))
+
+questions.forEach(question => question.addEventListener("click", () => {
+	if (question.classList.contains('cancel')) {
+		document.getElementById('cancel').classList.toggle('hidden')
+	}
+	if (question.classList.contains('trial')) {
+		document.getElementById('trial').classList.toggle('hidden')
+	}
+	if (question.classList.contains('change')) {
+		document.getElementById('change').classList.toggle('hidden')
+	}
+	if (question.classList.contains('security')) {
+		document.getElementById('security').classList.toggle('hidden')
+	}
+}))
